ca714c036e56307c18fbfd620cb1ae93977704f6
baerbaer <baerbaer@26dc1dd8-5c4e-0410-9ffe-d298b4865968>
2008-06-22 12:33:26 +0000

    Uncomment lines related to analytic solvent calculation.

--- trunk/energy.f
+++ trunk/energy.f
@@ -1,28 +1,28 @@
-c **************************************************************
-c
-c This file contains the subroutines: energy, enyinternal
-c
-c Copyright 2003-2005  Frank Eisenmenger, U.H.E. Hansmann,
-c                      Shura Hayryan, Chin-Ku 
-c Copyright 2007       Frank Eisenmenger, U.H.E. Hansmann,
-c                      Jan H. Meinke, Sandipan Mohanty
-c
-c **************************************************************
+! **************************************************************
+!
+! This file contains the subroutines: energy, enyinternal
+!
+! Copyright 2003-2005  Frank Eisenmenger, U.H.E. Hansmann,
+!                      Shura Hayryan, Chin-Ku 
+! Copyright 2007       Frank Eisenmenger, U.H.E. Hansmann,
+!                      Jan H. Meinke, Sandipan Mohanty
+!
+! **************************************************************
 
       real*8 function energy()
-c ------------------------------------------
-c PURPOSE: calculate the *total* energy of the system
-c
-c ientyp = 0 for ECEPP3, 1 for FLEX, 2 for Lund
-c          3 for ECEPP3 with Abagyan corrections
-c
-c CALLS:  enyflx,enyreg,enyshe,enysol,esolan,setvar, eninteract
-c
-c -------------------------------------------------
+! ------------------------------------------
+! PURPOSE: calculate the *total* energy of the system
+!
+! ientyp = 0 for ECEPP3, 1 for FLEX, 2 for Lund
+!          3 for ECEPP3 with Abagyan corrections
+!
+! CALLS:  enyflx,enyreg,enyshe,enysol,esolan,setvar, eninteract
+!
+! -------------------------------------------------
 
       include 'INCL.H'
       double precision teysm, teyel, teyvw, teyhb, teyvr
-c      print *,'energy function with ientyp  = ',ientyp   
+!      print *,'energy function with ientyp  = ',ientyp   
       esm = 0.d0
       teysm = 0.d0
       teyel = 0.d0
@@ -54,10 +54,10 @@ c      print *,'energy function with ientyp  = ',ientyp
         teyhb = teyhb + eyhb
         teyvr = teyvr + eyvr
         if (ientyp.eq.2) then
-c     The Lund term stores the hydrophobicity energy in eysl
+!     The Lund term stores the hydrophobicity energy in eysl
            teysl = teysl + eysl
         else 
-c     .. and the excluded volume term in eyvw, which is calculated once.
+!     .. and the excluded volume term in eyvw, which is calculated once.
            teyvw = teyvw + eyvw
         endif
 
@@ -66,25 +66,25 @@ c     .. and the excluded volume term in eyvw, which is calculated once.
       enddo
 
       if (ientyp.ne.2) then 
-c     Don't touch eysl if using Lund potential, as enylun stores 
-c     its hydrophobicity term there.
+!     Don't touch eysl if using Lund potential, as enylun stores 
+!     its hydrophobicity term there.
          if (itysol.gt.0) then
             esm=esm+enysol(0)
             teysl = teysl+eysl
-!     elseif (itysol.lt.0) then
-!     esm=esm+esolan(0)
+      elseif (itysol.lt.0) then
+      esm=esm+esolan(0)
          else
             eysl=0.d0
          endif
       else 
-c     Add excluded volume term and save it in eyvw
+!     Add excluded volume term and save it in eyvw
          esm=esm+exvlun(0)
          teyvw = teyvw+eyvw
       endif
 
-c The Abagyan entropic corrections depend on the area exposed to the 
-c solvent for each residue. So, this term has to be evaluated after the
-c solvent term.
+! The Abagyan entropic corrections depend on the area exposed to the 
+! solvent for each residue. So, this term has to be evaluated after the
+! solvent term.
       eyab=0.0
       if (ientyp.eq.3) then
          do i = 1,ntlml  
@@ -92,8 +92,8 @@ c solvent term.
          enddo
       endif
       esm=esm+eyab
-c Partial energies for the entire system. If you need the partial
-c energies for a single molecule call enyinternal.
+! Partial energies for the entire system. If you need the partial
+! energies for a single molecule call enyinternal.
       eysm = teysm
       eyel = teyel
       eyvw = teyvw
@@ -102,8 +102,8 @@ c energies for a single molecule call enyinternal.
       eysl = teysl
       
       if (ientyp.ne.2) then
-c     This is temporary. eninteract() does not yet know how to calculate
-c     interactions using the Lund potential.
+!     This is temporary. eninteract() does not yet know how to calculate
+!     interactions using the Lund potential.
          energy = esm + eninteract()
          return
       endif
@@ -111,17 +111,17 @@ c     interactions using the Lund potential.
       return
       end
 
-cc Calculates the internal energy for a single molecule.
-c  All the partial energies are thus set to their values for molecule
-c  nml. 
-c
-c  @param nml the ID of the molecule
-c  @return internal energy of a single molecule
-c
-c  @author Jan H. Meinke <j.meinke@fz-juelich.de>
+!c Calculates the internal energy for a single molecule.
+!  All the partial energies are thus set to their values for molecule
+!  nml. 
+!
+!  @param nml the ID of the molecule
+!  @return internal energy of a single molecule
+!
+!  @author Jan H. Meinke <j.meinke@fz-juelich.de>
       real*8 function enyinternal(nml)
 
-cf2py intent(in) nml
+!f2py intent(in) nml
       
       include 'INCL.H'
       esm = 0.d0
@@ -141,17 +141,17 @@ cf2py intent(in) nml
       if (ientyp.ne.2) then
          if (itysol.gt.0) then
             esm=esm+enysol(nml)
-!     elseif (itysol.lt.0) then
-!     esm=esm+esolan(nml)
+      elseif (itysol.lt.0) then
+      esm=esm+esolan(nml)
          else
             eysl=0.d0
          endif
       else 
          esm=esm+exvlun(nml)
       endif
-c The Abagyan entropic corrections depend on the area exposed to the 
-c solvent for each residue. So, this term has to be evaluated after the
-c solvent term.
+! The Abagyan entropic corrections depend on the area exposed to the 
+! solvent for each residue. So, this term has to be evaluated after the
+! solvent term.
       eyab=0.0
       if (ientyp.eq.3) then
          do i=1,ntlml
